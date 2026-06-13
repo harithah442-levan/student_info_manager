1482182b235b4993a589fd768c95a1241aee9713
HARITHA L S <harithah442@gmail.com>
2026-06-13 10:25:51 +0530

    Add files via upload

---
+++ student_info.py
@@ -0,0 +1,7 @@
+students = []
+for i in range(3):
+    name = input("Enter student name:")
+    students.append(name)
+print("\nstudents list:")
+print(students)
+print("Total students:", len(students))
